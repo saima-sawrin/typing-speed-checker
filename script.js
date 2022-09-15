db58c10f57dea6b41bdb91e0a1bd5ea9ab2e4ae2
Saima <saimasawrin324@gmail.com>
2022-09-15 10:52:29 +0600

    start button issue fixed

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const display = document.getElementById("display");
 const question = document.getElementById("question");
-const startBtn = document.getElementById("start");
+const startBtn = document.getElementById("starts");
 const countdownOverlay = document.getElementById("countdown");
 const resultModal = document.getElementById("result");
 const modalBackground = document.getElementById("modal-background");
@@ -10,6 +10,7 @@ let userText = "";
 let errorCount = 0;
 let startTime;
 let questionText = "";
+
 
 // Load and display question
 fetch("./texts.json")
@@ -107,12 +108,12 @@ const start = () => {
   countdownOverlay.style.display = "flex";
 
   const startCountdown = setInterval(() => {
-    countdownOverlay.innerHTML = '<h1>${count}</h1>';
+    countdownOverlay.innerHTML = `<h1> ${count} </h1>`;
 
     // finished timer
     if (count == 0) {
       // -------------- START TYPING -----------------
-      document.addEventListener("keydown", typeController);
+      startBtn.addEventListener("keydown", typeController);
       countdownOverlay.style.display = "flex";
       display.classList.remove("inactive");
 
@@ -121,10 +122,11 @@ const start = () => {
     }
     count--;
   }, 1000);
+  
 };
 
 // START Countdown
-startBtn.addEventListener("click", start);
+document.addEventListener("click", start);
 
 // If history exists, show it
 displayHistory();
